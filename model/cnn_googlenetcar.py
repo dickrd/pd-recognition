b7936457d1494aca098fb446ce353c6f88e79b3e
DickRD <dickdata7@gmail.com>
2017-11-02 16:04:28 +0800

    Bug fix.

--- model/cnn_googlenetcar.py
+++ model/cnn_googlenetcar.py
@@ -21,10 +21,11 @@ def build_googlecar(input_tensor, num_class, image_size, image_channel=3,
     mean = tf.reshape(mean, [1, 1, 1, 3])
     image_batch_sub_mean = input_tensor - mean
 
-    net = GoogleNet(inputs={"data": image_batch_sub_mean}, trainable=False)
+    net = GoogleNet(inputs={"input": image_batch_sub_mean}, trainable=False)
     with tf.Session() as sess:
         net.load(data_path=original_model, session=sess)
         layer_latest_conv = sess.graph.get_operation_by_name(name="pool5")
+        print "GoogleNet for cars load."
 
     with tf.variable_scope("custom_googlenet"):
         num_features = layer_latest_conv.get_shape()[1:].num_elements()
