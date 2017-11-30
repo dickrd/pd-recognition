de252fc459590a37571f87c46e071134cb1e1a86
YongSheng95 <pys954@163.com>
2017-11-30 16:10:43 +0800

    car color

--- data/image_loading.py
+++ data/image_loading.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from PIL import Image
 
 
@@ -100,3 +101,43 @@ def load_car_json_data(car_json_path, resize=(512, 512),
         image = image.resize(resize, Image.LANCZOS)
 
     return image, name
+
+def load_car_color_json_data(car_json_path,resize=(512,512),crop_percentage=1.0, name_filter=None):
+    """
+    load car json file.
+    ：param car_json_path: Path to a car json file.
+    : param resize: Resize image to this size.
+    : return: PIL.Image object of the car's image and the label(car color).
+    """
+    import json
+    import base64
+    import io
+    from PIL import Image
+    ColorList = [u"白",u"灰",u"黄",u"粉",u"红",u"紫",u"绿",u"蓝",u"棕",u"黑"]
+    with open(car_json_path,'r') as json_data:
+        j = json.load(json_data)
+        #Color of this image
+        color = j["color"]
+        if color:
+            color = color.split("#")[0]
+            #color = color.decode('utf-8')
+            if color[-1] in ColorList:
+                color = color[-1]
+            elif color[-1] == u"银":
+                color = u"灰"
+            else:
+                flag = True
+                for v in color:
+                    if v in ColorList:
+                        color = v
+                        flag = False
+                if flag:
+                    color =u"其他"
+
+            image_bytes = base64.b64decode(j["image"])
+            image = Image.open(io.BytesIO(image_bytes)).convert(mode="RGB")
+            image = image.resize(resize,Image.LANCZOS)
+        else:
+            return None,None
+
+    return image,color
--- model/cnn.py
+++ model/cnn.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from model.common import build_cnn, optimize, ConfusionMatrix
 
@@ -84,9 +85,13 @@ def predict(model_path, name_dict, feed_image, feed_image_size, feed_image_chann
     print "Loading image from {0} with size: {1}x{1}.".format(feed_image, feed_image_size)
     image = load_image_data(image_path=feed_image,
                             resize=(feed_image_size, feed_image_size))
+
+    #image = tf.cast(image, tf.float32)
+    #image = np.reshape(np.array(image),(1,feed_image_size,feed_image_size,feed_image_channel))
+    image = np.expand_dims(image, axis=0)
     with tf.Graph().as_default():
         # Input placeholder.
-        x = tf.placeholder(tf.float32, shape=[feed_image_size, feed_image_size, 3], name='image_flat')
+        x = tf.placeholder(tf.float32, shape=[1,feed_image_size, feed_image_size, 3], name='image_flat')
         y, y_pred_cls = build(input_tensor=x, num_class=len(name_dict),
                               image_size=feed_image_size, image_channel=feed_image_channel)
         y_pred = tf.nn.softmax(y)
