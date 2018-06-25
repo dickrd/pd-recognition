2e8b385f251db0e70e96e226947da604ddb7ade3
DickRD <dickdata7@gmail.com>
2018-06-25 15:32:32 +0800

    add single img process

--- data/align_face.py
+++ data/align_face.py
@@ -12,6 +12,7 @@ def pipeline(input_path, aligned_path):
         output_path = path.replace(input_path, os.path.join(aligned_path, "aligned/"), 1)
         try:
             os.makedirs(face_path)
+            os.makedirs(output_path)
         except OSError:
             pass
 
@@ -24,6 +25,34 @@ def pipeline(input_path, aligned_path):
                                          is_align=True,
                                          is_draw_fidu=True,
                                          delete_no_fidu=True)
+
+
+def process_img(img_path):
+    output_path = "aligns/"
+    try:
+        os.makedirs(output_path)
+    except OSError:
+        pass
+
+    cascade_face_aligner = CascadeFaceAligner()
+    faces_file = cascade_face_aligner.face_finder.create_faces_file(img_path, is_overwrite=False, target_file='aligns/faces.txt')
+    sub_images_files = cascade_face_aligner.face_finder.create_sub_images_from_file(original_image_file=img_path,
+                                                                                    faces_file=faces_file,
+                                                                                    target_folder=output_path,
+                                                                                    img_type='jpg')
+    cascade_face_aligner.align_faces(input_images=sub_images_files,
+                                     output_path=output_path,
+                                     fidu_max_size=200 * 200,
+                                     fidu_min_size=50 * 50,
+                                     is_align=True,
+                                     is_draw_fidu=True,
+                                     delete_no_fidu=True)
+
+    result_img_path = []
+    for img_file in sub_images_files:
+        _, img_name = os.path.split(img_file)
+        result_img_path.append(os.path.join(output_path, img_name.rsplit('.',1)[0] + '.aligned.png'))
+    return result_img_path
 
 
 if __name__ == "__main__":
